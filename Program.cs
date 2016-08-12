26cb304bc45fd945e2664aeee945d6959f24526c
Chris Ballance <github@ballance.it>
2016-08-12 00:03:03 -0400

    Simple example connecting to mysql from .net core using Dappe

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Data.SqlClient;
+using Dapper;
+using MySql.Data.MySqlClient;
 
 namespace ConsoleApplication
 {
@@ -7,11 +8,26 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var connectionString = "";
-            using (var connection = new SqlConnection(connectionString))
+            Console.WriteLine("Connecting for database listing...");
+            try
             {
-                
+                var connectionString = "Server=127.0.0.1;Database=adventureworks;Uid=dapper;Pwd=DapDap1$";
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    var simpleResult = connection.Query("SELECT CustomerID, TerritoryId, AccountNumber, CustomerType, ModifiedDate FROM customer LIMIT 10;");
+                    foreach (var result in simpleResult)
+                    {
+                        Console.WriteLine($"{result.CustomerID} - {result.CustomerID} - {result.AccountNumber}");
+                    }
+                }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to connect {ex}");
+            }
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
         }
     }
 }
