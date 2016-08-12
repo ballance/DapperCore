ad1efb682f67d7c0806e109b127bbf33904cb7f5
Chris Ballance <github@ballance.it>
2016-08-11 23:41:55 -0400

    initial chicken

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = "";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                
+            }
+        }
+    }
+}
